81b6ead1bfc3cfc362bb3fe872779a6c85624cde
Jang Insoo <oasisholics@gmail.com>
2021-08-24 18:04:37 +0900

    공지사항 크롤러 테스트 코드 작성 -> 크롤러 테스터는 어떻게 작성할지 몰라서 클래스만 작성해둠

---
+++ src/test/java/com/insoo/jang/webcrawler/web/NoticeCrawlerApiControllerTest.java
@@ -0,0 +1,32 @@
+package com.insoo.jang.webcrawler.web;
+
+import com.insoo.jang.webcrawler.domain.crawling.NoticeCrawlerRepository;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class NoticeCrawlerApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private NoticeCrawlerRepository noticeCrawlerRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        noticeCrawlerRepository.deleteAll();
+    }
+}
